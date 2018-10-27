cc8b04c6b356dc367ca8ca56bb56ccc6791b2d6e
Noclip <noclip@foxmail.com>
2018-10-27 16:03:45 +0800

    计时器、计分器

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var game = new Phaser.Game('100', '100', Phaser.AUTO, '', {
     update: update
 })
 
-var roaches, corpse, ang = 0, qian = 0
+var roaches, corpse, ang = 0, qian = 0, countdown = 60, score = 0, countdown_text = '', score_text = ''
 function preload () {
     game.load.spritesheet('roach', 'images/roach.png', 64, 64, 4)
 }
@@ -23,8 +23,22 @@ function create () {
     
     roaches.inputEnabled = true;
     roaches.events.onInputDown.add(clickRoach, this);
+
+    countdown_text = game.add.text(10, 10, countdown)
+    score_text = game.add.text(game.width - 50, 10, score)
+    timer = game.time.create()
+    timer.start()
 }
 function update () {
+    passtime = Math.round((timer.ms)/1000)
+    if (timer.running) {
+        if (passtime <= countdown) {
+            countdown_text.setText(countdown - passtime)
+        } else {
+            timer.stop()
+            console.log('Game over')
+        }
+    }
     if (qian) {
         ang += 5
         qian --
@@ -39,7 +53,9 @@ function hitWorldBounds() {
     qian = 5
 }
 function clickRoach() {
+    if (!timer.running) return false
     roaches.kill()
+    score_text.setText(++score)
     corpse = game.add.sprite(roaches.x, roaches.y, 'roach', 3);
     corpse.anchor.setTo(0.5, 0.5);
     corpse.angle = roaches.angle
